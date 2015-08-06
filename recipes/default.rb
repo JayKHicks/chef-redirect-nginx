f1b7594083a3900bf5cd76e9edcc38144338a629
JayKHicks <jhicks@gmti.gannett.com>
2015-08-06 12:22:38 -0400

    GDPHAR-872 added a role and override configuration values

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
+node.override['nginx']['server_names_hash_bucket_size'] = '512'
+node.override['nginx']['user'] = 'nobody'
+node.override['nginx']['types_hash_max_size'] = '8192'
+node.override['nginx']['types_hash_bucket_size'] = '256'
+node.override['nginx']['status']['port'] = '8080'
+
 include_recipe 'nginx::default'
 
 # cookbook_file "#{node['nginx']['dir']}/redirects.conf" do
